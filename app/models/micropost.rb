6931f4cadc23fcb95dce90fe678ac246d6f9db81
Alex808r <fromxlam@gmail.com>
2021-06-18 20:50:20 +0300

    [RC-2] Add has_many

--- app/models/micropost.rb
+++ app/models/micropost.rb
@@ -1,4 +1,6 @@
 class Micropost < ApplicationRecord
   #Валидация длинны сообщения максимум 140. поле content из таблицы базы данных 
   validates :content, length: { maximum: 140 }
+
+  belongs_to :user
 end
