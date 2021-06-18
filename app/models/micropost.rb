90ea17f7b5af1a8ee8d7194e4b21d09e10b025ff
Alex808r <fromxlam@gmail.com>
2021-06-18 21:01:17 +0300

    [RC-2] Add validate to User

--- app/models/micropost.rb
+++ app/models/micropost.rb
@@ -1,6 +1,6 @@
 class Micropost < ApplicationRecord
   #Валидация длинны сообщения максимум 140. поле content из таблицы базы данных 
-  validates :content, length: { maximum: 140 }
+  validates :content, length: { maximum: 140 }, presence: true
 
   belongs_to :user
 end
